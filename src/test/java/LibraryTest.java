d00419fb6b1ddcdaf0f08e6148c875bad0faeb89
Atharv1903 <awishkar193@gmail.com>
2024-09-27 09:56:24 +0530

    Added Feature of "Adding Book"

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -11,5 +11,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class LibraryTest {
-    
+    private Library library;
+
+    @BeforeEach
+    public void setUp() {
+        library = new Library();
+    }
+
+    @Test
+    public void testAddBook() {
+        Book book = new Book("978-3-16-148410-0", "The Great Gatsby", "F. Scott Fitzgerald", 1925);
+        library.addBook(book);
+        System.out.println("Book added: " + book);
+        assertEquals(1, library.getAvailableBooks().size(), "Book was not added to the library.");
+        System.out.println("Total available books: " + library.getAvailableBooks().size());
+    }
 }
